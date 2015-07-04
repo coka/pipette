0c089eca891ce5d53cdffa5e9bd3ab034ba177a0
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-04 18:15:30 +0200

    Users now have unique usernames.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   has_many :comments
   has_many :snippets
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
 end
