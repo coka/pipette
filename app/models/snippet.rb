d46e4bb7771172a8230a3ceb9975794e0391745a
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-08 13:37:55 +0200

    Add initial Snippet specs.

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,16 @@
 class Snippet < ActiveRecord::Base
-  belongs_to :user
+
   has_many :comments, :dependent => :destroy
+  belongs_to :user
+
+  validates_presence_of(:user_id)
+
+  validates :title,
+    :length => {
+      :minimum => 4,
+      :maximum => 64,
+      :too_short => "is too short (minimum is %{count} characters)",
+      :too_long => "is too long (maximum is %{count} characters)"
+    }
+
 end
