97b7b2843b28914afaa2c8d4a60811cda2df3d50
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-08 13:39:52 +0200

    Add initial Comment specs.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :snippet
+
+  validates_presence_of(:user_id)
+  validates_presence_of(:snippet_id)
+
+  validates :content,
+    :presence => :true,
+    :length => {
+      :maximum => 512,
+      :too_long => "is too long (maximum is %{count} characters)"
+    }
 end
