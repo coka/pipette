1d7be50462f2a9c1783ae98c56743b30b262e3e7
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-08 18:29:54 +0200

    Encapsulate ownership check.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def owned_by_current_user?(object)
+    if user_signed_in?
+     object.user_id == current_user.id
+   end
+  end
+
 end
--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,6 @@
 class Snippet < ActiveRecord::Base
+
   belongs_to :user
   has_many :comments, :dependent => :destroy
+
 end
