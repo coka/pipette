dba13dd601cea9eb97770db53671a700b666201d
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-14 16:57:23 +0200

    Restore SnippetsController, and improve test coverage.

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,7 +1,14 @@
 class SnippetsController < ApplicationController
 
+  before_action :set_snippet, :only => [:show, :edit, :update, :destroy]
+
   def index
     @snippets = Snippet.all.order("created_at DESC")
+  end
+
+  def show
+    @comment = @snippet.comments.build
+    @comments = @snippet.comments.all
   end
 
   def new
@@ -9,12 +16,35 @@ class SnippetsController < ApplicationController
   end
 
   def create
-    @snippet = current_user.snippets.build(params[:snippet])
+    @snippet = current_user.snippets.build(snippet_params)
     if @snippet.save
       redirect_to :action => "index"
     else
       render :action => "new"
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @snippet.update(params[:snippet])
+    redirect_to :action => "index"
+  end
+
+  def destroy
+    @snippet.destroy
+    redirect_to :action => "index"
+  end
+
+  private
+
+  def set_snippet
+    @snippet = Snippet.find(params[:id])
+  end
+
+  def snippet_params
+    params.require(:snippet).permit(:title, :content)
   end
 
 end
