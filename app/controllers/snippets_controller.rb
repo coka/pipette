5ba960d288713b24812918cb05d644b05de60658
Bojan Čoka <metropolis.lights@gmail.com>
2015-07-14 16:17:36 +0200

    Initial SnippetsController spec.

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,47 +1,12 @@
 class SnippetsController < ApplicationController
 
-  before_action :set_snippet, :only => [:show, :edit, :update, :destroy]
-
-  def index
-    @snippets = Snippet.all.order("created_at DESC")
-  end
-
-  def show
-    @comment = @snippet.comments.build
-    @comments = @snippet.comments.all
-  end
-
-  def new
-    @snippet = current_user.snippets.build
-  end
-
   def create
-    @snippet = current_user.snippets.build(snippet_params)
-    @snippet.save
-    redirect_to snippets_path
-  end
-
-  def edit
-  end
-
-  def update
-    @snippet.update(snippet_params)
-    redirect_to snippets_path
-  end
-
-  def destroy
-    @snippet.destroy
-    redirect_to snippets_path
-  end
-
-  private
-
-  def set_snippet
-    @snippet = Snippet.find(params[:id])
-  end
-
-  def snippet_params
-    params.require(:snippet).permit(:title, :content)
+    snippet = Snippet.new(params[:snippet])
+    if snippet.save
+      redirect_to :action => "index"
+    else
+      render :action => "new"
+    end
   end
 
 end
